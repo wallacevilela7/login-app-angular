df1ff028a0505587369638f07e2ac485f0bcb73a
wallacevilela7 <wallace.vilela9@gmail.com>
2025-03-25 17:05:45 -0300

    feat: addd default layout style

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-default-login-layout',
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class DefaultLoginLayoutComponent {
 
+    @Input() title: string = "";
+    @Input() primaryButtonText: string = "";
+    @Input() secondaryButtonText: string = "";
 }
