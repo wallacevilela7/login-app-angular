87c68422e8a019b023fd239acc30c54c9a5e71c2
wallacevilela7 <wallace.vilela9@gmail.com>
2025-03-25 14:34:08 -0300

    feat: implement base structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LoginpageComponent} from "./pages/loginpage/loginpage.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginpageComponent
+    }
+];
---
+++ src/app/pages/loginpage/loginpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {DefaultLoginLayoutComponent} from "../../components/default-login-layout/default-login-layout.component";
+
+@Component({
+  selector: 'app-loginpage',
+  imports: [DefaultLoginLayoutComponent],
+  templateUrl: './loginpage.component.html',
+  styleUrl: './loginpage.component.scss'
+})
+export class LoginpageComponent {
+
+}
