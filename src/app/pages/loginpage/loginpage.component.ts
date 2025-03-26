1df32845781cadc861738225ea04d12b3a869147
wallacevilela7 <wallace.vilela9@gmail.com>
2025-03-26 18:12:15 -0300

    feat: add primary input component and integrate into login page

--- src/app/pages/loginpage/loginpage.component.ts
+++ src/app/pages/loginpage/loginpage.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import {DefaultLoginLayoutComponent} from "../../components/default-login-layout/default-login-layout.component";
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-loginpage',
-  imports: [DefaultLoginLayoutComponent],
+  imports: [DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.scss'
 })
 export class LoginpageComponent {
 
+  loginForm!:  FormGroup;
+  
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    })
+  }
 }
